ad3258a87109d9c42a68bacce3acbb4e1dec211e
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-05-07 10:59:31 +0700

    added check of args + kwargs

--- app/main.py
+++ app/main.py
@@ -9,15 +9,16 @@ def cache(func: Callable) -> Callable:
     def inner(*args, **kwargs) -> Callable:
         fname = func.__name__
         nonlocal res_storage
-        if fname not in res_storage or args not in [
+        inputs = args, kwargs
+        if fname not in res_storage or inputs not in [
             el[0] for el in res_storage[fname]
         ]:
             print("Calculating new result")
             res = func(*args, **kwargs)
-            res_storage[fname] = res_storage.get(fname, []) + [(args, res)]
+            res_storage[fname] = res_storage.get(fname, []) + [(inputs, res)]
         else:
             for el in res_storage[fname]:
-                if el[0] == args:
+                if el[0] == inputs:
                     print("Getting from cache")
                     res = el[1]
                     break
